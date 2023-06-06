83c11d4480974bfad5400157f6b4c64ef0e02800
andrebnassis <andrebnassis@gmail.com>
2023-06-06 12:55:47 -0300

    change settings and popup info

--- foreground.js
+++ foreground.js
@@ -4,8 +4,6 @@
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
-
 const displayTargetElement = (elem, shouldDisplay) => {
     if (shouldDisplay) {
         elem.style.display = 'block';
